fa611d1b1fdd027505c27c5a9150104cb36cf062
Quentin Fayet <quentin.fayet@reputationvip.com>
2016-03-11 09:30:46 +0100

    Fix case in test files

--- tests/units/Adapter/NullAdapter.php
+++ tests/units/Adapter/NullAdapter.php
@@ -9,73 +9,73 @@ class NullAdapter extends atoum\test
 
     public function testNullAdapterAddMessage()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->class($NullAdapter->addMessage('testQueue', 'test Message one'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->class($nullAdapter->addMessage('testQueue', 'test Message one'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
     }
 
     public function testNullAdapterAddMessages()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->class($NullAdapter->addMessages('testQueue', ['test Message one']))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->class($nullAdapter->addMessages('testQueue', ['test Message one']))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
     }
 
     public function testNullAdapterGetMessages()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->array($NullAdapter->getMessages('testQueue'))->isEmpty();
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->array($nullAdapter->getMessages('testQueue'))->isEmpty();
     }
 
     public function testNullAdapterDeleteMessage()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->class($NullAdapter->deleteMessage('testQueue', []))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->class($nullAdapter->deleteMessage('testQueue', []))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
     }
 
     public function testNullAdapterIsEmpty()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->boolean($NullAdapter->isEmpty('testQueue'))->isTrue();
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->boolean($nullAdapter->isEmpty('testQueue'))->isTrue();
     }
 
     public function testNullAdapterGetNumberMessages()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->integer($NullAdapter->getNumberMessages('testQueue'))->isEqualTo(0);
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->integer($nullAdapter->getNumberMessages('testQueue'))->isEqualTo(0);
     }
 
     public function testNullAdapterDeleteQueue()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->class($NullAdapter->deleteQueue('testQueue'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->class($nullAdapter->deleteQueue('testQueue'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
     }
 
     public function testNullAdapterCreateQueue()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->class($NullAdapter->createQueue('testQueue'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->class($nullAdapter->createQueue('testQueue'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
     }
 
     public function testNullAdapterRenameQueue()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->class($NullAdapter->renameQueue('testQueue', 'newTestQueue'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->class($nullAdapter->renameQueue('testQueue', 'newTestQueue'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
     }
 
     public function testNullAdapterPurgeQueue()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->class($NullAdapter->purgeQueue('testQueue'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->class($nullAdapter->purgeQueue('testQueue'))->hasInterface('\ReputationVIP\QueueClient\Adapter\AdapterInterface');
     }
 
     public function testNullAdapterListQueues()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->array($NullAdapter->listQueues())->isEmpty();
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->array($nullAdapter->listQueues())->isEmpty();
     }
 
     public function testNullAdapterGetPriorityHandler()
     {
-        $NullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
-        $this->given($NullAdapter)->class($NullAdapter->getPriorityHandler())->hasInterface('\ReputationVIP\QueueClient\PriorityHandler\PriorityHandlerInterface');
+        $nullAdapter = new \ReputationVIP\QueueClient\Adapter\NullAdapter();
+        $this->given($nullAdapter)->class($nullAdapter->getPriorityHandler())->hasInterface('\ReputationVIP\QueueClient\PriorityHandler\PriorityHandlerInterface');
     }
 }
